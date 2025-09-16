193b4dae5d94858ef7db9516f3ea6e3f66027e7a
sav <aevs.andres.vidal@gmail.com>
2025-09-16 15:34:51 -0600

    feat: add launch file for the app

---
+++ TasksApp/TasksApp.cs
@@ -0,0 +1,46 @@
+﻿using DotNetEnv;
+using TasksApp.Controllers;
+
+namespace TasksApp
+{
+    internal class TasksApp
+    {
+        static async Task Main()
+        {
+            Env.Load();
+
+            var option = 0;
+
+            do
+            {
+                Console.WriteLine(
+                "1. Show task list\n" +
+                "2. Add task\n" +
+                "3. Update task\n" +
+                "4. Remove task\n" +
+                "5. Exit\n"
+                );
+
+                string? value = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Please, choose an option from the list.\n");
+                    continue;
+                }
+
+                if (!int.TryParse(value, out int newValue))
+                {
+                    Console.WriteLine("\nOption undefined...\n");
+                    option = 0;
+                    continue;
+                }
+                
+                option = newValue;
+
+                await TaskController.ExecuteOption(option);
+            } while (option != 5);
+            
+        }
+    }
+}
