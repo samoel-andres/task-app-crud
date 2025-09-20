91037ab97e8065183ad8b1ff9a63363fdcea6be1
sav <aevs.andres.vidal@gmail.com>
2025-09-19 22:06:50 -0600

    feat: add task model

---
+++ TasksApp/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TasksApp.Models
+{
+    internal class TaskModel
+    {
+        internal required int Id { get; init; }
+        internal required string Title { get; init; }
+        internal required string Description { get; init; }
+        internal required bool Completed { get; init; }
+        internal required DateTime CreationDate { get; init; }
+    }
+}
